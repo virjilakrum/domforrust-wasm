317ed17ebe1be02c4c2bf81b81450263d78a7788
Virjilakrum <asesenep15@gmail.com>
2024-07-28 17:32:00 +0300

    move |_: ClickEvent|  The { part defines a closure.  It will listen for events of type ClickEvent and the closer will run when this event is triggered.

--- dom/src/app1.rs
+++ dom/src/app1.rs
@@ -1,2 +1,8 @@
 use stdweb::web::ClickEvent;
 use stdweb::web::{document, window, IEventTarget};
+
+pub fn main() {
+    let btn = document().query_selector("#app1 button").unwrap().
+        btn.add_event_listener( move |_: ClickEvent| {
+            window().alert("assalamu alaykum, world!");
+}
