d8054f7aeb2966ade3f0abc014eaf79326615602
Virjilakrum <asesenep15@gmail.com>
2024-07-28 18:14:16 +0300

    Create app2.rs

---
+++ dom/src/app2.rs
@@ -0,0 +1,13 @@
+use stdweb::web::{document, INode};
+
+pub fn main() {
+    let div = document().query_selector("#app2");
+    if let Some(div) = div {
+        for i in 0..5 {
+            let li = document().create_element("li");
+            let s = String::from("label ") + i.to_string().as_str();
+            li.append_child(&document().create_text_node(&s));
+            div.append_child(&li);
+        }
+    }
+}
